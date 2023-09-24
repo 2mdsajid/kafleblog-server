caa5e3c30c812d787726b87120e7e803a3fab43b
sajid aalam <2mdsajid@gmail.com>
2023-09-24 21:26:53 +0545

    dsad

--- db/mongo.js
+++ db/mongo.js
@@ -4,13 +4,12 @@ const mongoose = require("mongoose")
 // DeprecationWarning:
 mongoose.set('strictQuery', true)
 
-// const DB = `mongodb+srv://2mdsajid:${process.env.MONGOPASS}@cluster0.c1pm0ml.mongodb.net/blog2?retryWrites=true&w=majority`
-const DB = `mongodb://0.0.0.0:27017/kafle`
+const DB = `mongodb+srv://2mdsajid:${process.env.MONGOPASS}@cluster0.c1pm0ml.mongodb.net/blog2?retryWrites=true&w=majority`
+// const DB = `mongodb://0.0.0.0:27017/kafle`
 
 mongoose.connect(DB).then(()=>{
     console.log('connected successfully to blog2 database');
 }).catch((err) => {
-    console.log("🚀 ~ file: mongo.js:13 ~ mongoose.connect ~ err:", err)
     console.log('error while connecting to blog2 database')
 
 })
